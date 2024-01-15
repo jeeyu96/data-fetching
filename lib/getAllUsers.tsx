e29cda6cee8d63408897a7276421af3b3217f36a
Jee-Yu <jeeyu96@gmail.com>
2024-01-15 17:21:18 +0100

    basic fetching

--- lib/getAllUsers.tsx
+++ lib/getAllUsers.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default async function getAllUsers() {
 
     // create the fetch 
-  return (
-    <div>getAllUsers</div>
-  )
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    if (!res.ok) throw new Error('failed to fetch data')
+
+  return res.json()
 }
--- users/page.tsx
+++ users/page.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next"
+import getAllUsers from "@/lib/getAllUsers"
 
 export const metadata: Metadata = {
     title: 'Users',
 }
 
 export default function UsersPage() {
+    // will request this during the build -> this is a static website
+    const UsersData: Promise<User[]> = getAllUsers()
   return (
     <div>page</div>
   )
