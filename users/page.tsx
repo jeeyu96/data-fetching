9a634a548679796f09a7c32bdf17472e29fe74db
Jee-Yu <jeeyu96@gmail.com>
2024-01-15 17:09:53 +0100

    basic page

--- users/page.tsx
+++ users/page.tsx
@@ -1,1 +1,11 @@
 import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Users',
+}
+
+export default function UsersPage() {
+  return (
+    <div>page</div>
+  )
+}
