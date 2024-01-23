dad4e69343d062fbd1189fe1025ce622dd1e2520
Jee-Yu <jeeyu96@gmail.com>
2024-01-23 17:52:40 +0100

    remove styles of pages

--- users/page.tsx
+++ users/page.tsx
@@ -1,14 +1,39 @@
 import type { Metadata } from "next"
 import getAllUsers from "@/lib/getAllUsers"
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: 'Users',
 }
 
-export default function UsersPage() {
+export default async function UsersPage() {
     // will request this during the build -> this is a static website
-    const UsersData: Promise<User[]> = getAllUsers()
-  return (
-    <div>page</div>
-  )
+    const usersData: Promise<User[]> = getAllUsers()
+
+    const users = await usersData
+
+    console.log('Hello')
+
+    // map over what we expect to receive
+    const content = (
+      <section>
+        <h2>
+          <Link href="/"> Back to Home </Link>
+        </h2>
+        <br />
+        {users.map(user => {
+          return (
+            <>
+              <p key={user.id}>
+                <Link href={`/users/${user.id}`}> {user.name} </Link>
+              </p>
+              <br />
+            </>
+          )
+        })}
+      </section>
+    )
+
+  return content
 }
+
